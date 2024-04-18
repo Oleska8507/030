b24a595380144f4f37f3ab3f0e60f72cd70b3148
Oleska8507 <163408283+Oleska8507@users.noreply.github.com>
2024-04-19 00:33:14 +0300

    Update db.php  deleted

--- Shop/php/db.php
+++ Shop/php/db.php
@@ -1,9 +1,6 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 
-$host = "localhost";
-$db = "k96537xl_030";
-$user = "k96537xl_030";
-$password = "qwerty1A";
+
 
 $mysqli = mysqli_connect($host, $user, $password, $db);
